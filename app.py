ff9c6b7e802677aabba92404e1e82f7a5f8dc78d
salvestacia <darrensiwibu@gmail.com>
2023-09-21 19:12:20 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,17 @@
+#hitung luas segitiga sederhana
+def luas_segitiga():
+    a = int(input("masukkan alas segitiga: "))
+    t = int(input("masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("luas segitiga adalah: ", luas)
+
+luas_segitiga()
+
+#hitung luas persegi panjang
+def luas_persegi_panjang():
+    p = int(input("masukkan panjang persegi: "))
+    l = int(input("masukkan lebar persegi: "))
+    luas = p * l
+    print("luas persegi panjang adalah: ", luas)
+
+luas_persegi_panjang()
