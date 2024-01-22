ee7e27db87d8c056e053c811b8e0b45e33fa11ee
salvestacia <144323961+salvestacia@users.noreply.github.com>
2024-01-22 21:30:09 +0700

    python code to make calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+x = input()
+y = input()
+
+a = float(x)
+b = float(y)
+
+penjumlahan = a + b
+pengurangan = a - b
+perkalian = a * b
+pembagian = a / b
+modulo = a % b
+print("hasilnya adalah:    ", penjumlahan)
+print("hasilnya adalah:    ", pengurangan)
+print("hasilnya adalah:    ", perkalian)
+print("hasilnya adalah:    ", pembagian)
+print("hasilnya adalah:    ", modulo)
